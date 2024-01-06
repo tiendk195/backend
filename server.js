c450b8eaccc75ea9314629f0170c84346ab66176
tiendk195 <tiendk195@gmail.com>
2024-01-06 15:16:56 +0700

    pushv2

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const hostname = "localhost";
+const port = 8080;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+});
+server.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}/`);
+});

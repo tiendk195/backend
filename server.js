47c799ca3bc3d67779e1865071106949a731fc1a
tiendk195 <tiendk195@gmail.com>
2024-01-12 08:35:35 +0700

    push code 12/1/2024

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const http = require("http");
-const hostname = "localhost";
-const port = 8080;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8081;
+const hostname = process.env.HOST_NAME;
+app.get("/", (req, res) => {
+  res.send(`Tien Coder`);
 });
-server.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}/`);
+
+app.get("/hello", (req, res) => {
+  // res.send(`Hello TienCoder `);
+  res.render(`sample.ejs`);
 });
+app.listen(port, hostname, () => {
+  console.log(`Server is running at ${port}`);
+});
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
